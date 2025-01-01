7f95b07159ab314f9e3141bffe2b40b712f2a728
Wallace Costa <holandawallacecosta@gmail.com>
2025-01-01 06:54:06 -0300

    Atualiza __init__.py com importações principais e metadados

---
+++ src/__init__.py
@@ -0,0 +1,8 @@
+"""
+Pacote para análise de vendas de pequenos negócios.
+
+Este pacote inclui módulos para manipulação, análise e visualização de dados de vendas.
+"""
+
+__version__ = "1.0.0"
+__author__ = "Wallace de Holanda Costa"
